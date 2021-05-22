4eaae875ed4f091d33759c365908cc88a695d2fc
Gabriel Souza <gabriel.souza@targetwork.com.br>
2021-05-22 18:44:29 -0300

    criando aplicação e criando People e Film model

---
+++ src/app/models/Film.ts
@@ -0,0 +1,29 @@
+import { People } from "./people";
+
+export class Film {
+    constructor(title: string, episode_id: number, opening_crawl: string, director: string, producer: string,
+                release_date: Date, characters: People[], url: string, created: string, edited: string){
+        
+        this.title = title;
+        this.episode_id = episode_id;
+        this.opening_crawl = opening_crawl;
+        this.director = director;
+        this.producer = producer;
+        this.release_date = release_date;
+        this.characters = characters;
+        this.url = url;
+        this.created = created;
+        this.edited = edited;
+    }
+
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: Date;
+    characters: People[];
+    url: string;
+    created: string;
+    edited: string;
+}
---
+++ src/app/models/People.ts
@@ -0,0 +1,35 @@
+import { Film } from "./Film";
+
+export class People {
+    constructor(name: string, birth_year: string, eye_color: string, gender: string, hair_color: string,
+                height: string, mass: string, skin_color: string, homeworld: string, url: string,
+                created: string, edited: string, films: Film[]) {
+        this.name = name;
+        this.birth_year = birth_year;
+        this.eye_color = eye_color;
+        this.gender = gender;
+        this.hair_color = hair_color;
+        this.height = height;
+        this.mass = mass;
+        this.skin_color = skin_color;
+        this.homeworld = homeworld;
+        this.url = url;
+        this.created = created;
+        this.edited = edited;
+        this.films = films;
+    }
+
+    name: string;
+    birth_year: string;
+    eye_color: string;
+    gender: string;
+    hair_color: string;
+    height: string;
+    mass: string;
+    skin_color:string;
+    homeworld: string;
+    films: Film[];
+    url: string
+    created: string;
+    edited: string;
+}
